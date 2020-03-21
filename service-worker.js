bf12a2098235e221f4ce8540a093d9e3d8ea06a6
skyzhou <zhouhaomin019@gmail.com>
2020-03-21 17:05:03 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SkyZhou197/tomato-alarm-clock/login/precache-manifest.e4bdc7a27f37d0a62785c8d590aeb2ce.js"
+  "/SkyZhou197/tomato-alarm-clock/precache-manifest.0d0f37c9599ee5fafddee034a19275f4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SkyZhou197/tomato-alarm-clock/login/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SkyZhou197/tomato-alarm-clock/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
